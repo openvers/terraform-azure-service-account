890d025823706d5a2e76ee2b818a45a254cce6b8
retrhinkr-hub <jason.grein@gmail.com>
2024-05-27 16:41:10 -0400

    Unscrambled integration workflow jobs

--- test/service_account/output.tf
+++ test/service_account/output.tf
@@ -1,14 +1,17 @@
 output "service_account_client_id" {
   description = "Azure Service Account Client ID"
   value       = module.azure_service_account.client_id
+  sensitive   = true
 }
 
 output "service_account_client_secret" {
   description = "Azure Service Account Client Secret"
   value       = nonsensitive(module.azure_service_account.client_secret)
+  sensitive   = true
 }
 
 output "service_account_application_id" {
   description = "Azure Service Account Application ID"
   value       = module.azure_service_account.application_id
+  sensitive   = true
 }
